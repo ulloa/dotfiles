14716f4f19e47f027f062b98306bdb972cb976fe
ulloa <samuel.ulloa@gmail.com>
2018-09-05 08:25:03 -0700

    Removed more unneeded packages and files from repo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,20 +52,8 @@
 ;; ------------
 ;; -- Macros --
 ;; ------------
-(load "~/.emacs.d/defuns-config.el")
-(fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
-(global-set-key "\M-=" 'align-equals)
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c;" 'comment-or-uncomment-region)
-(global-set-key "\M-n" 'next5)
-(global-set-key "\M-p" 'prev5)
-(global-set-key "\M-o" 'other-window)
-(global-set-key "\M-i" 'back-window)
-(global-set-key "\C-z" 'zap-to-char)
-(global-set-key "\C-h" 'backward-delete-char)
-(global-set-key "\M-d" 'delete-word)
-(global-set-key "\M-h" 'backward-delete-word)
-(global-set-key "\M-u" 'zap-to-char)
+;; See balajis defuns-config for some. I didn't use any of the so I took them all out
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
