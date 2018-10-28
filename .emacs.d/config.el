601ee06772cb7123d2c46f38c33115034a0a016a
sam <samuel.ulloa@gmail.com>
2018-10-27 21:53:51 -0700

    Removed the disable for line-columns spaceline

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -318,8 +318,8 @@
 :config
 (require 'spaceline-config)
 (setq spaceline-buffer-encoding-abbrev-p nil)
-(setq spaceline-line-column-p nil)
-(setq spaceline-line-p nil)
+;;(setq spaceline-line-column-p nil)
+;;(setq spaceline-line-p nil)
 (setq powerline-default-separator (quote arrow))
 (spaceline-spacemacs-theme))
 
